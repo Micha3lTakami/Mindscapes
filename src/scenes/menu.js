709c37848e24f92ac87e4b4b8da20b37ce4b3d68
kickflipyeah <59582785+kickflipyeah@users.noreply.github.com>
2023-05-24 11:14:19 -0700

    fixed error

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -4,16 +4,7 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        window.WebFont.load({
-            google: {
-                families: [ 'Caveat', 'Foldit', 'Bruno Ace', 'Rubik Pixels' ]
-            },
-            
-       
-        
-        
-        
-            let text = this.add.text(game.config.width/2, game.config.height/2, 'menu');
+        let text = this.add.text(game.config.width/2, game.config.height/2, 'menu');
         let next = this.add.text(game.config.width/2, game.config.height*0.65, 'Press Enter to change to play scene');
         this.add.image(0, 0, 'title_screen').setOrigin(0, 0);
         
