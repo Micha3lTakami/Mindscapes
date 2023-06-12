f2b55b7ef0216456f8c6a9ddee96047dd37bf195
jpenlan <79890843+jpenlan@users.noreply.github.com>
2023-06-11 23:02:44 -0700

    gravity test

--- src/prefabs/gravity.js
+++ src/prefabs/gravity.js
@@ -42,6 +42,10 @@ class GravityBlock extends Phaser.Physics.Arcade.Sprite {
     player.body.setAcceleration(force.x, force.y);
   }
 
+  removeGravity(player) {
+    player.body.setAcceleration(0, 0);
+  }
+
   onClick() {
     // Delete the block
     
