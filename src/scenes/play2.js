47d1593c7aea45e678702bd294598571c881261a
jpenlan <79890843+jpenlan@users.noreply.github.com>
2023-06-11 21:47:39 -0700

    Level 2 done

--- src/scenes/play2.js
+++ src/scenes/play2.js
@@ -22,7 +22,7 @@ class Play2 extends Phaser.Scene {
         //gameMusic.play();
 
         // reset the availablePlatforms value
-        availablePlatforms = 10;
+        availablePlatforms = 3;
         
         
         //add map tilesprite
@@ -281,7 +281,7 @@ class Play2 extends Phaser.Scene {
         if (availablePlatforms > 0) {
           const worldX = pointer.worldX;
           const worldY = pointer.worldY;  
-          const newPlat = new platform(this, worldX, worldY, 'platform').setOrigin(0.5);
+          const newPlat = new JumpBlock(this, worldX, worldY, 'jumpBlock').setOrigin(0.5);
           //newPlat.setScale(0.75);
           availablePlatforms--;
           console.log('avail plats: ' + availablePlatforms)
